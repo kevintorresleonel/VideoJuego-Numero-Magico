4a4238dab499ce0e83feb6b9e52a848d247764d4
Kevin Torres <kevintorresleonel.17@gmail.com>
2025-03-19 10:27:47 -0500

    agregación de mensajes de guia al usuario y numero alleatorio

--- JS/logica.js
+++ JS/logica.js
@@ -6,3 +6,30 @@ function cambiarVista(idVista) {
     // Mostrar la vista seleccionada
     document.getElementById(idVista).style.display = "block";
 }
+
+
+var numeroMagico = Math.floor(Math.random()*100+1); 
+console.log("numeroMagico"); 
+
+function comprobarNumero(botonComprobar){
+
+    const node = document.createElement("p"); 
+const textnode = document.createTextNode("El número que ingresaste es mayor al número magico"); 
+
+    let numUsuario = document.getElementById("numUsuario").value;
+    let mensaje = document.getElementById("mensaje").value;
+    if(numeroMagico > numUsuario){
+        const textnode = document.createTextNode("El número que ingresaste es mayor al número magico"); 
+    }
+    if(numeroMagico < numUsuario){
+        const textnode = document.createTextNode("El número que ingresaste es menor al número magico"); 
+    }
+    if(numeroMagico == numUsuario){
+        const textnode = document.createTextNode("El número que ingresaste es el correcto, ¡FELICIDADES!"); 
+    }
+    
+    node.appendChild(textnode); 
+
+    document.getElementById("mensaje").appendChild(node); 
+
+}
